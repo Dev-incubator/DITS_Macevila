aafd275e6c8634240262033801f1e0bb9dca9394
AlexandrSviridenkoTMS09 <91564222+AlexandrSviridenkoTMS09@users.noreply.github.com>
2022-11-17 17:07:36 +0300

    Done level 4-5. I combinated this level's, because they were related to tests and testing. Found and fixed bug with file html.

--- src/main/java/com/example/dits/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/dits/service/impl/UserServiceImpl.java
@@ -27,7 +27,13 @@ public class UserServiceImpl implements UserService {
 
     @Transactional
     public UserInfoDTO getUserInfoById(Integer id) {
-        return modelMapper.map(repository.getUserByUserId(id), UserInfoDTO.class);
+        User user = repository.getUserByUserId(id);
+
+        if (user != null) {
+            return modelMapper.map(user, UserInfoDTO.class);
+        }
+
+        return new UserInfoDTO();
     }
 
     @Override
@@ -48,5 +54,22 @@ public class UserServiceImpl implements UserService {
     @Override
     public void save(User user) {
         repository.save(user);
+    }
+
+    @Override
+    public void update(User user, Integer id) {
+        if (repository.findById(id).isPresent()) {
+            repository.save(user);
+        }
+    }
+
+    @Override
+    public void delete(User user) {
+        repository.delete(user);
+    }
+
+    @Override
+    public List<User> findAll() {
+        return repository.findAll();
     }
 }
