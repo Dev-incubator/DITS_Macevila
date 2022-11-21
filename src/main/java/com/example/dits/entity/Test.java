1e4ac87a37ec382db757e090d27a50177ddf4b7f
AlexandrSviridenkoTMS09 <91564222+AlexandrSviridenkoTMS09@users.noreply.github.com>
2022-11-21 15:52:34 +0300

    Done level 6 + fixed bugs.

--- src/main/java/com/example/dits/entity/Test.java
+++ src/main/java/com/example/dits/entity/Test.java
@@ -17,7 +17,7 @@ import java.util.List;
 public class Test {
 
     @Column
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @Id
     private int testId;
 
